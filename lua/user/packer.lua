ef62aeacaeb0697d6be4e2201daf4421b682911a
Artem Musalitin <musalitin.a.a@gmail.com>
2023-08-29 19:55:52 +0400

    Update packer.lua

--- lua/user/packer.lua
+++ lua/user/packer.lua
@@ -9,7 +9,7 @@ return require('packer').startup(function(use)
 
   use({
 	  'nvim-telescope/telescope.nvim', tag = '0.1.2',
-	  -- or                            , branch = '0.1.x',
+	  -- or                            , branch = '1.1.x',
 	  requires = { {'nvim-lua/plenary.nvim'} }
   })
   use({ 'rose-pine/neovim', as = 'rose-pine' })
@@ -54,7 +54,8 @@ return require('packer').startup(function(use)
     'nvim-lualine/lualine.nvim',
     requires = { 'nvim-tree/nvim-web-devicons', opt = true }
   }),
-  use("lukas-reineke/indent-blankline.nvim")
+  use("lukas-reineke/indent-blankline.nvim"),
+  use("ledger/vim-ledger"),
 })
 end)
 
