421163071a64417796460b0e3daa10b998a2b856
Artem Musalitin <artem@musalitin.me>
2024-10-28 19:23:17 +0100

    Change theme, use lualine

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,33 +1,44 @@
 return {
-	{ -- You can easily change to a different colorscheme.
-		-- Change the name of the colorscheme plugin below, and then
-		-- change the command in the config to whatever the name of that colorscheme is.
-		--
-		-- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-		"folke/tokyonight.nvim",
-		priority = 1000, -- Make sure to load this before all the other start plugins.
-		init = function()
-			-- Load the colorscheme here.
-			-- Like many other themes, this one has different styles, and you could load
-			-- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-			vim.cmd.colorscheme("tokyonight")
-
-			-- You can configure highlights by doing something like:
-			vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopeBorder", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopePromptBorder", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = "none" })
-			vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { bg = "none" })
-		end,
+	{
+		"Tsuzat/NeoSolarized.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other start plugins
 		config = function()
-			require("tokyonight").setup({
-				transparent = true,
-				styles = {
-					sidebars = "transparent",
-					floats = "transparent",
+			vim.cmd([[ colorscheme NeoSolarized ]])
+			require("lualine").setup({
+				options = {
+					theme = "NeoSolarized",
+				},
+			})
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("lualine").setup({
+				options = {
+					component_separators = { left = " | ", right = " | " },
+					section_separators = { left = " ", right = " " },
+				},
+				sections = {
+					lualine_a = { "mode" },
+					lualine_b = {
+						"branch",
+						"diff",
+						"diagnostics",
+						{
+							"filename",
+							file_status = true,
+							newfile_status = false,
+							path = 1,
+							shorting_target = 80,
+						},
+					},
+					lualine_c = {},
+					lualine_x = { "encoding", "filetype" },
+					lualine_y = {},
+					lualine_z = { "location", "progress" },
 				},
 			})
 		end,
