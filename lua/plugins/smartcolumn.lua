6809521442e6d310bddd50497687512bcfc5bf68
Artem Musalitin <artem@musalitin.me>
2025-02-12 11:08:59 +0100

    Add smart column, update plugins

---
+++ lua/plugins/smartcolumn.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "m4xshen/smartcolumn.nvim",
+    opts = {
+      colorcolumn = { "80", "120" },
+      disabled_filetypes = {
+        "netrw",
+        "NvimTree",
+        "lazy",
+        "mason",
+        "help",
+        "checkhealth",
+        "lspinfo",
+        "noice",
+      },
+    },
+  },
+  {
+    "lukas-reineke/virt-column.nvim",
+    opts = {},
+  },
+}
