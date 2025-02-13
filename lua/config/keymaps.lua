4785b8717d28b44c8132f07bcd0c9389e125f1f0
Artem Musalitin <artem@musalitin.me>
2025-02-13 11:05:59 +0100

    Add keymap for copy filepath

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,5 +2,10 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+local map = LazyVim.safe_keymap_set
+
 -- Example:
 -- map({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save File" })
+
+map({ "n" }, "<leader>fp", '<cmd>let @+ = expand("%")<CR>', { desc = "Copy file path to clipboard" })
+
