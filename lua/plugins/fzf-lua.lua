ae9f9500e83913c1eb3963243f87d01d8e5fc8eb
Artem Musalitin <artem@musalitin.me>
2025-02-14 16:18:58 +0100

    Change fzf-lua picker opts

---
+++ lua/plugins/fzf-lua.lua
@@ -0,0 +1,13 @@
+return {
+  "ibhagwan/fzf-lua",
+  dependencies = { "echasnovski/mini.icons" },
+  opts = {
+    winopts = {
+      fullscreen = true,
+      preview = {
+        layout = "vertical",
+        vertical = "down:50%"
+      }
+    }
+  }
+}
